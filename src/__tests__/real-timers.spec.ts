d449a86129cdf1f97067d978d00d164aca1b3c6b
xobotyi <xog3@yandex.ru>
2019-09-05 18:34:06 +0300

    fix: prettier expects doubleqotes on file template

--- src/__tests__/real-timers.spec.ts
+++ src/__tests__/real-timers.spec.ts
@@ -9,12 +9,12 @@ test('calls timer', async () => {
 
   expect(spy).toHaveBeenCalledTimes(0);
 
-  await new Promise(r => setTimeout(r, 5));
+  await new Promise((r) => setTimeout(r, 5));
   expect(spy).toHaveBeenCalledTimes(1);
 
   clearHarmonicInterval(ref);
 
-  await new Promise(r => setTimeout(r, 6));
+  await new Promise((r) => setTimeout(r, 6));
   expect(spy).toHaveBeenCalledTimes(1);
 });
 
@@ -23,13 +23,13 @@ test('calls all callbacks at the same time, and clears', async () => {
   const spy2 = jest.fn();
 
   const ref1 = setHarmonicInterval(spy1, 30);
-  await new Promise(r => setTimeout(r, 15));
+  await new Promise((r) => setTimeout(r, 15));
   const ref2 = setHarmonicInterval(spy2, 30);
 
   expect(spy1).toHaveBeenCalledTimes(0);
   expect(spy2).toHaveBeenCalledTimes(0);
 
-  await new Promise(r => setTimeout(r, 16));
+  await new Promise((r) => setTimeout(r, 16));
 
   expect(spy1).toHaveBeenCalledTimes(1);
   expect(spy2).toHaveBeenCalledTimes(1);
@@ -37,7 +37,7 @@ test('calls all callbacks at the same time, and clears', async () => {
   clearHarmonicInterval(ref1);
   clearHarmonicInterval(ref2);
 
-  await new Promise(r => setTimeout(r, 31));
+  await new Promise((r) => setTimeout(r, 31));
 
   expect(spy1).toHaveBeenCalledTimes(1);
   expect(spy2).toHaveBeenCalledTimes(1);
